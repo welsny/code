7905f83e905272f125f6b92a079ceb71e264b522
welsny <zengw@mssm.org>
2016-08-05 13:15:40 -0400

    improve the code :+1:

--- gpa.py
+++ gpa.py
@@ -1,19 +1,27 @@
-SCORE = {'A': 4,
-        'A-':3.66667,
-        'B+':3.33333,
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+SCORE = {'A':4,
+        'A-':3.667,
+        'B+':3.333,
          'B':3,
-        'B-':2.66667,
-        'C+':2.33333,
-         'C':2}
+        'B-':2.667,
+        'C+':2.333,
+         'C':2,
+        'C-':1.667,
+        'D+':1.333,
+         'D':1,
+        'D-':0.667,
+         'F':0}
 
 data = open('grades').read()
-
 raw = data.split('\n')[1:-1]
 
-print raw
+credits = [int(row.split(',')[0]) for row in raw]
+points = [SCORE[row.split(',')[1]] for row in raw]
 
-total_credits = sum(int(row.split(',')[0]) for row in raw)
-total_points  = sum(SCORE[row.split(',')[1]]*int(row.split(',')[0]) for row in raw)
+total_credits = sum(credits)
+total_points  = sum(c*p for (c,p) in zip(credits, points))
 
 print total_points / total_credits
 
